90c2f58f396ca5f29cf403f3881b5021d3ffb10a
thailshe160614 <thailshe160614@fpt.edu.vn>
2022-10-01 23:23:58 +0700

    front end forget password

--- Project/src/java/controller/ResetServlet.java
+++ Project/src/java/controller/ResetServlet.java
@@ -5,6 +5,7 @@
 
 package controller;
 
+import DAO.AccountDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -12,6 +13,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 
 /**
@@ -69,7 +71,15 @@ public class ResetServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        AccountDAO ad= new AccountDAO();
+        HttpSession session= request.getSession();
+        String email= (String) request.getAttribute("mail");
+        if(ad.getAccByEmail(email)==null){
+            session.setAttribute("mess", "Invalid email! Please try again!");
+            request.getRequestDispatcher("forgetpassword.jsp").forward(request, response);
+        }else{
+            
+        }
     }
 
     /** 
