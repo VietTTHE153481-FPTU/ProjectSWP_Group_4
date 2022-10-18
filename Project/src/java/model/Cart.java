1eed44fd2a4ac9fac8ea0292e65f069c9bdf877f
Minh <112998452+minhtche161354@users.noreply.github.com>
2022-10-18 23:11:31 +0700

    cart

---
+++ Project/src/java/model/Cart.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Minhm
+ */
+public class Cart {
+
+    public item hold;
+
+}
+
+class item {
+
+    public Products mid;
+    public int numO;
+}
