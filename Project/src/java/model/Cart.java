c660329dc0dd718ff82d6579c75a3f7a6d0a3639
Minh <112998452+minhtche161354@users.noreply.github.com>
2022-10-19 00:29:36 +0700

    chia cart ra lam 2 servlet xem va add

--- Project/src/java/model/Cart.java
+++ Project/src/java/model/Cart.java
@@ -59,34 +59,4 @@ public class Cart {
     }
 }
 
-class item {
 
-    public Products item_product;
-    public int numO;
-
-    public item() {
-
-    }
-
-    public item(Products p, int num) {
-        this.item_product = p;
-        this.numO = num;
-    }
-
-    public Products getItem_product() {
-        return item_product;
-    }
-
-    public void setItem_product(Products item_product) {
-        this.item_product = item_product;
-    }
-
-    public int getNumO() {
-        return numO;
-    }
-
-    public void setNumO(int numO) {
-        this.numO = numO;
-    }
-
-}
